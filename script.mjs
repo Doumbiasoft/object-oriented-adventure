ca93a6c52689dd0e08380df865fd6572ca1b8e32
Mouhamed Doumbia <doumbiasoft@gmail.com>
2025-08-20 15:21:00 -0400

    implemented second part

--- script.mjs
+++ script.mjs
@@ -48,6 +48,7 @@ Next, we will level up our approach using Classes.
  * 
  * 
  * ***/
+console.log(`\n--------------------------FIRST PART--------------------------`);
 const adventurer = {
   name: "Robin",
   health: 10,
@@ -58,11 +59,78 @@ const adventurer = {
     companion: {
       name: "Frank",
       type: "Flea",
-      belongings: ["small hat", "sunglasses"],
+      inventory: ["small hat", "sunglasses"],
     },
+  },
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
   },
 };
 
 adventurer.inventory.forEach((ele) => {
   console.log(ele);
 });
+adventurer.roll();
+adventurer.roll();
+adventurer.roll();
+
+/***
+ * 
+Part 2: Class Fantasy
+Let’s take a moment to analyze the data above. What are the common features of each object?
+When creating classes, begin by searching for the simplest form your data takes. Remember, you can add specificity later by extending the classes.
+Start with a Character class, which will define generic character entities. Robin and their companions all have a name, so the Character class should definitely include name as one of its properties. At this stage, we will also make the decision that every character should have health (which we will standardize to a maximum of 100, and an inventory (even if the inventory is empty).
+Here is what the basic Character class looks like so far, including a constructor function that allows us to create new characters with whatever name we would like:
+class Character {
+  constructor (name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
+  }
+}
+
+Every character should also be able to make rolls. Add the roll method to the Character class.
+Now, we can re-create Robin using the Character class!
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+Not only does this allow us to create standardized objects for each character, it also ensures that they all have common properties and methods such as roll(). Even the companions can roll now; give it a try! This saves us a significant amount of typing since we do not need to manually add this method to each nested object.
+While progress has been made, this is still not the most efficient way to create these objects. In order to effectively create companions, we need to extend the Character class for added specificity.
+ * 
+ * 
+ * 
+ * * */
+console.log(
+  `\n--------------------------SECOND PART--------------------------`
+);
+
+class Character {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
+  }
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  }
+}
+
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+robin.roll();
+robin.roll();
+robin.roll();
+
+console.log(robin);
