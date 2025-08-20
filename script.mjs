1cb317c6ca842acda9a116c40cc0bdac55d9ae29
Mouhamed Doumbia <doumbiasoft@gmail.com>
2025-08-20 14:52:04 -0400

    definition of companion in adventurer and in companion object

--- script.mjs
+++ script.mjs
@@ -52,6 +52,15 @@ const adventurer = {
   name: "Robin",
   health: 10,
   inventory: ["sword", "potion", "artifact"],
+  companion: {
+    name: "Leo",
+    type: "Cat",
+    companion: {
+      name: "Frank",
+      type: "Flea",
+      belongings: ["small hat", "sunglasses"],
+    },
+  },
 };
 
 adventurer.inventory.forEach((ele) => {
